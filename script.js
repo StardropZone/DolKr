216452549acc35c92256ac4bacc6e622732d4525
Eva <toto941002@gmail.com>
2024-06-21 13:24:12 +0900

    test versionChecker
    버전체커 테스트

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//최신 버전 체커
+
 document.addEventListener("DOMContentLoaded", function() {
     // 버전 정보를 가져와서 index.html에 표시하는 함수
     function updateVersion() {
@@ -42,34 +45,34 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('version-status').appendChild(updateButton);
     }
 
-// 업데이트 자동화 워크플로우 트리거 함수
-function triggerUpdateWorkflow() {
-    fetch('https://api.github.com/repos/StardropZone/DolKr/actions/workflows/update.yml/dispatches', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': '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' // 여기에 Personal Access Token을 입력하세요
-        },
-        body: JSON.stringify({
-            "ref": "main"
+    // 업데이트 자동화 워크플로우 트리거 함수
+    function triggerUpdateWorkflow() {
+        fetch('https://api.github.com/repos/StardropZone/DolKr/actions/workflows/update.yml/dispatches', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/vnd.github.v3+json',
+                'Authorization': `token ${process.env.GITHUB_TOKEN}` // 환경 변수에서 Personal Access Token을 가져옵니다.
+            },
+            body: JSON.stringify({
+                "ref": "main"
+            })
         })
-    })
-    .then(response => {
-        const updateStatus = document.createElement('p');
-        if (response.ok) {
-            updateStatus.textContent = "업데이트 완료!";
-        } else {
+        .then(response => {
+            const updateStatus = document.createElement('p');
+            if (response.ok) {
+                updateStatus.textContent = "업데이트 완료!";
+            } else {
+                updateStatus.textContent = "업데이트 실패...";
+            }
+            document.getElementById('version-status').appendChild(updateStatus);
+        })
+        .catch(error => {
+            console.error('Error triggering the update workflow:', error);
+            const updateStatus = document.createElement('p');
             updateStatus.textContent = "업데이트 실패...";
-        }
-        document.getElementById('version-status').appendChild(updateStatus);
-    })
-    .catch(error => {
-        console.error('Error triggering the update workflow:', error);
-        const updateStatus = document.createElement('p');
-        updateStatus.textContent = "업데이트 실패...";
-        document.getElementById('version-status').appendChild(updateStatus);
-    });
-}
+            document.getElementById('version-status').appendChild(updateStatus);
+        });
+    }
 
     // 기능 초기화 함수 호출
     updateVersion();
@@ -77,3 +80,4 @@ function triggerUpdateWorkflow() {
     // 체크 버전 버튼에 이벤트 리스너 추가
     document.getElementById('check-version-button').addEventListener('click', checkLatestVersion);
 });
+
