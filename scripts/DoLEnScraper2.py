a84890f23fa5c058471baec81cf9dd090dc0a185
Eva <toto941002@gmail.com>
2024-06-23 20:53:07 +0900

    update AutoUpdateEn
    -

--- scripts/DoLEnScraper2.py
+++ scripts/DoLEnScraper2.py
@@ -13,9 +13,10 @@ def download_file(url):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/local/bin/google-chrome"
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.binary_location = "/usr/bin/google-chrome"
     
-    service = Service("/usr/local/bin/chromedriver")
+    service = Service("/usr/bin/chromedriver")
     
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
