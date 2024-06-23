1c7b92441b27e80175fbd5e8daed11fb70230ae2
Eva <toto941002@gmail.com>
2024-06-24 01:35:13 +0900

    update AutoUpdateEn
    -

--- scripts/DoLEnScraper2.py
+++ scripts/DoLEnScraper2.py
@@ -45,9 +45,9 @@
 import sys
 import subprocess
 
-def download_file_from_mega(download_url):
+def download_file_from_mega(download_url, output_filename):
     # Mega CLI 명령어 실행
-    command = f"megadl {download_url}"
+    command = f"megadl --path {output_filename} {download_url}"
     try:
         subprocess.run(command, shell=True, check=True)
         print("다운로드 성공.")
@@ -55,11 +55,13 @@ def download_file_from_mega(download_url):
         print(f"다운로드 실패: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python DoLEnScraper2.py <download_url>")
+    if len(sys.argv) != 3:
+        print("Usage: python DoLEnScraper2.py <download_url> <output_filename>")
         sys.exit(1)
     
     download_url = sys.argv[1]
-    download_file_from_mega(download_url)
+    output_filename = sys.argv[2]
+    download_file_from_mega(download_url, output_filename)
+
 
 
